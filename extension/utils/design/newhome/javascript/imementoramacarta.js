9177ed74696dbc3634f528e270944d5bde09e4cd
jsuero <jesus.suero@tantacom.com>
2013-04-10 17:28:50 +0200

    [EFL-70]

---
+++ extension/utils/design/newhome/javascript/imementoramacarta.js
@@ -0,0 +1,177 @@
+(function ($) {
+
+var prettyChecks = {
+		init:function(){
+			
+			$("input[type='checkbox'].pretty").each(function(){
+				var $that = $(this),
+					$parent = $that.parent(),
+					clase;
+					
+					$that.addClass("off");
+
+					if($that.is(":checked")){
+						clase="c_on";
+					}else{
+						clase="c_off";
+					}
+
+					$parent.addClass(clase)
+			
+
+				
+			}).click(function(){
+				var $that = $(this),
+					$parent = $that.parent(),
+					clase;
+
+				if($parent.hasClass("c_on")){
+					$that.removeAttr("checked");
+					$parent.removeClass("c_on").addClass("c_off");
+				}else{
+					$that.attr("checked","checked");
+					$parent.removeClass("c_off").addClass("c_on");
+				}
+			})
+
+		}
+
+	}
+
+var imemento = {
+	clickControl:function(){
+		var obj = $(".tryPromo");
+		$(".tryProd").click(function () { 
+			$(this).toggleClass("sel"); 
+			$("#frm_tryImemento").toggle(); 
+			$(".msgError", obj).remove();
+			$(".error", obj).removeClass("error")
+			return false;
+		});
+	}
+}
+
+$('#productlist').infinitescroll({
+	 
+    navSelector  : "div.pag",            
+                   // selector for the paged navigation (it will be hidden)
+    nextSelector : "div.pag a",    
+                   // selector for the NEXT link (to page 2)
+    contentSelector : "#table-rows",
+    itemSelector : "#productlist > .imementos tbody tr"          
+                   // selector for all items you'll retrieve
+  });
+
+
+
+ function disableChecks(valor)
+	{
+		
+		$('input[type=checkbox]').each(function () {
+				$(this).attr("disabled", true);
+			});
+	}
+	
+	function enableChecks(valor)
+	{
+		
+		$('input[type=checkbox]').each(function () {
+				$(this).attr("disabled", false);
+			});
+	} 
+  
+
+function checkImementoPrice( )
+    {
+    var n = $("#productlist input:checked").length;
+	
+		
+		if (n == 1){
+                $("#modMiImementoInt").text(n + ' ' + literal["mementos"][2]);
+        }else{
+                $("#modMiImementoInt").text(n + ' ' + literal["mementos"][3]);
+        }
+				
+		var values = new Array();
+		$.each($("input[name='mementos[]']:checked"), function() {
+		  values.push($(this).val());
+		  
+		});
+			
+			$.get( '/basket/imementoramacheckprice',
+				{ mementos: n, id : $("#object").val(), products : values.join(',') },
+				function(data){
+				pintaCesta(data);	
+			}, 'json');
+			
+			
+	}
+ 
+function removetoBasket(object)
+{
+	
+	if (!(object.checked))
+	{
+		product = $("#ProductItemIDList_"+ object.value);
+		$.get('/basket/ajaxremove/'+product.val()+'/1', function(data) 
+				{
+					$("#infocesta").html( data.output );
+                }, 'json');
+		
+	}
+	
+} 
+ 
+function pintaCesta(data)
+{
+	$("#preload").hide();
+	$("#addToBasket").show();
+	enableChecks();
+	var div = "";
+	$.each(data.row, function(index, value) 
+		{
+			div += '<p>Nombre: '+value.name+'</p><p>Precio: <del><span id="partial">'+value.price+'</span></del></p><p>Precio: <ins><span id="ptotal">'+value.total+'</span></ins></p><p class="discount">Descuento: <span id="dtotal">'+value.discount+'</span></p>';
+			div += '<div class="sepBasket"></div>';	
+			product = $("#ProductItemIDList_"+ value.id);
+			product.attr("value",value.remove);
+			texto = (value.items==1) ? value.items + " producto" : value.items + " productos";
+			$("#infocesta").html('Tiene <a href="/basket/basket">'+texto+'</a> en la cesta');
+		});
+		
+	$("#basketAdd").html(div);	
+} 
+  
+
+	$(document).ready( function()
+	{
+
+	
+		imemento.clickControl();
+	
+        if($("#preload").length != 0) $("#preload").hide();
+		
+			
+		if($("#productlist input").length != 0){	
+      		$("#productlist input").click(function(){
+				
+				$("#addToBasket").hide();
+				$("#preload").show();
+				disableChecks(this);
+				checkImementoPrice();
+				removetoBasket(this);
+				
+				
+		})};    	
+        $("#mementosForm").submit( function() {    
+            var n = $("#productlist input:checked").length;            
+            if(n == 0){
+            	alert( 'Debe seleccionar al menos un Pack' );
+               return false;
+            }
+        });
+		
+		if($("input.pretty").length){prettyChecks.init();}
+		if($(".filter").length){filter.init();}
+		
+    });
+})(jQuery);
