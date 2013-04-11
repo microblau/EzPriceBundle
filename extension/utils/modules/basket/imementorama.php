ae04a405aa5be53b3711104fcb5855eebaf2cd69
jesus suero <jesus.suero@tantacom.com>
2013-04-11 12:04:27 +0200

    arreglado pete cuando no hay basket

--- extension/utils/modules/basket/imementorama.php
+++ extension/utils/modules/basket/imementorama.php
@@ -8,25 +8,27 @@ $content = array();
 $removeitem = array();
 $productos = array();
 
-foreach ($basket->items() as $item)
+if ($basket->Session)
 {
-	$content[$item["item_object"]->ContentObject->ID] = $item["item_object"]->ContentObject->ID;
-	$removeitem[$item["item_object"]->ContentObject->ID] = $item["id"];
-	
-	$pData = eZContentObject::fetch( $item["item_object"]->ContentObject->ID );
-	$datos = $pData->dataMap();
+	foreach ($basket->items() as $item)
+	{
+		$content[$item["item_object"]->ContentObject->ID] = $item["item_object"]->ContentObject->ID;
+		$removeitem[$item["item_object"]->ContentObject->ID] = $item["id"];
+		
+		$pData = eZContentObject::fetch( $item["item_object"]->ContentObject->ID );
+		$datos = $pData->dataMap();
 
-	$precio = $datos['precio']->content()->attribute( 'ex_vat_price' );
-	$total =  $datos['precio_oferta']->content()->attribute( 'ex_vat_price' );
-	$discountpercent = $datos['descuento_pack']->content();
-	$name = $datos['nombre']->content();
-	$productos[] = array('precio' => $precio, 'total' => $total, 'discountpercent'=> $discountpercent, 'name' => $name);
-}
+		$precio = $datos['precio']->content()->attribute( 'ex_vat_price' );
+		$total =  $datos['precio_oferta']->content()->attribute( 'ex_vat_price' );
+		$discountpercent = $datos['descuento_pack']->content();
+		$name = $datos['nombre']->content();
+		$productos[] = array('precio' => $precio, 'total' => $total, 'discountpercent'=> $discountpercent, 'name' => $name);
+	}
 
 $tpl->setVariable( "content", $content );
 $tpl->setVariable( "removeitem", $removeitem );
 $tpl->setVariable( "productos", $productos );
-
+}
 
 $Result['content'] = $tpl->fetch( 'design:basket/imementorama.tpl' );
 $Result['path'] = array( 
