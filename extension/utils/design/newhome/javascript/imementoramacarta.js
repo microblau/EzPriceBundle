fe19995fdb0db223211c47f0a491b31a7540c4d0
sergiocibanto <sergio.cibanto@tantacom.com>
2013-04-22 11:50:29 +0200

    prueba3 scroll infinito

--- extension/utils/design/newhome/javascript/imementoramacarta.js
+++ extension/utils/design/newhome/javascript/imementoramacarta.js
@@ -154,7 +154,7 @@ var fixedBox = {
 var infiniteScroll = {
 	init:function(){
 		$("table.imementos tbody tr").addClass("hide").hide();
-		$("table.imementos tbody tr:lt(4)").show();
+		$("table.imementos tbody tr:lt(4)").show().removeClass("hide");;
 		$("table.imementos tbody tr:lt(4)").addClass("show");
 		
 		$(window).endlessScroll({
