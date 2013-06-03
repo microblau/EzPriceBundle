8aef5a32d39679bbbae0ec6202ee27e7a6f591c6
sergiocibanto <sergio.cibanto@tantacom.com>
2013-06-03 18:53:50 +0200

    quitar fixed a cesta con mas de dos elementos

--- extension/utils/design/newhome/javascript/imementoramacarta.js
+++ extension/utils/design/newhome/javascript/imementoramacarta.js
@@ -281,13 +281,14 @@ function pintaCesta(data)
 			checkImementoPrice( $("#valor").val() );
 			
 			var n = $("#productlist input:checked").length;
-			if($("modMiImemento").length != 0){
+			alert(n);
+			/*if($("modMiImemento").length != 0){
 				if(n > 2){
 					$("modMiImemento").removeClass("fixed");
 				}else{
 					$("modMiImemento").addClass("fixed");
 				}
-			}
+			}*/
 		});
 				
         $("#mementosForm").submit( function() {    
