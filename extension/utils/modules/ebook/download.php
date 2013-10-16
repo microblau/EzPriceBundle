0321f2229741f1573f6af4ae7fb1c39aaafd7eb4
crevillo <carlos.revillo@tantacom.com>
2013-10-16 18:17:43 +0200

    fix descarga ebook

--- extension/utils/modules/ebook/download.php
+++ extension/utils/modules/ebook/download.php
@@ -15,8 +15,16 @@ if( !$object = eZContentObject::fetch( $objectID ) )
     return $Module->handleError( eZError::KERNEL_NOT_AVAILABLE, 'kernel' );
 }
 $data = $object->dataMap();
+if( isset( $data['archivo' ] ) ) {
 $file =  $data['archivo']->content()->attribute( 'filepath' );
 $original =  $data['archivo']->content()->attribute( 'original_filename' );
+}
+else{
+$file =  $data['file']->content()->attribute( 'filepath' );
+$original =  $data['file']->content()->attribute( 'original_filename' );
+
+}
+
 
 header('Content-disposition: attachment; filename=' . $original );
 header('Content-type: application/pdf');
