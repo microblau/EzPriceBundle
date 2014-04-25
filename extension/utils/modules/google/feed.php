786418af1d95981be3abf9e457119d4fcc70f4ee
Carlos Revillo <carlos.revillo@tantacom.com>
2014-04-25 09:37:11 +0200

    feed productos google

--- extension/utils/modules/google/feed.php
+++ extension/utils/modules/google/feed.php
@@ -9,8 +9,9 @@ $products = eZContentObjectTreeNode::subTreeByNodeId(
 $xml = new DOMDocument( "1.0" );
 $xml->formatOutput = TRUE;
 $root = $xml->createElement('rss');
-$root->setAttribute('version', '2.0' );
 $root->setAttribute('xmlns:g', "http://base.google.com/ns/1.0");
+$root->setAttribute('version', '2.0' );
+
 
 $channel = $xml->createElement('channel');
 $channel_title = $xml->createElement('title', 'Ediciones Francis Lefebvre' );
@@ -30,13 +31,14 @@ $item_title = $xml->createElement('title' );
 $item_title->appendChild( $item_title_cdata );
 $item_link = $xml->createElement('link', 'http://www.efl.es/' . $product->urlAlias() );
 $data = $product->dataMap();
-print_r(array_keys($precio) );
+
 $item_description_cdata = $xml->createCDATASection( $data['entradilla']->content()->attribute('output')->attribute('output_text') );
 $item_description = $xml->createElement('description' );
 $item_description->appendChild( $item_description_cdata);
 
 $item_id = $xml->createElement( 'g:id', 'EFL-' . $product->attribute('node_id' ) );
 $item_condition = $xml->createElement( 'g:condition', 'new' );
+$item_price = $xml->createElement( 'g:price', number_format( $data['precio']->content()->attribute('inc_vat_price'), 2 )  . ' EUR' );
 
 
 $item->appendChild( $item_title );
@@ -44,6 +46,37 @@ $item->appendChild( $item_link );
 $item->appendChild( $item_description );
 $item->appendChild( $item_id );
 $item->appendChild( $item_condition );
+$item->appendChild( $item_price );
+if ( $data['precio']->content()->attribute('has_discount' )) {
+	$item_offer_price = $xml->createElement( 'g:sale_price', number_format( $data['precio']->content()->attribute('discount_price_inc_vat'), 2 )  . ' EUR' );
+	$item->appendChild( $item_offer_price );
+}
+
+$item_availabilty = $xml->createElement('g:availability', 'in_stock' );
+$item->appendChild( $item_availabilty );
+
+
+if( $image = $data['imagen']->content() ){
+if( $img_object = eZContentObject::fetch( $image['relation_browse']['0']['contentobject_id'] ) ){
+$img_object_data = $img_object->dataMap();
+$aliasList = $img_object_data['image']->content()->aliasList();
+
+$fullPath = $aliasList['original']['full_path'];
+
+
+$item_image_link = $xml->createElement('g:image_link', 'http://www.efl.es/' . $fullPath );
+$item->appendChild( $item_image_link );
+}
+}
+
+$isbn = $data['isbn']->content();
+$item_gtin = $xml->createElement('g:gtin', $isbn['value_without_hyphens']);
+$item->appendChild( $item_gtin );
+
+$item_type_cdata = $xml->createCDATASection('Medios audiovisuales > Libros > No ficción > Manuales de derecho');
+$item_type = $xml->createElement('g:product_type'  );
+$item_type->appendChild( $item_type_cdata );
+$item->appendChild($item_type );
 
 $channel->appendChild($item);
 
@@ -52,8 +85,10 @@ $channel->appendChild($item);
 $root->appendChild($channel);
 
 $xml->appendchild($root);
-header('Content-Type: text/xml');
-echo $xml->saveXML();
+header('Content-disposition: attachment; filename="efl_feed.xml"');
+header('Content-type: "text/xml"; charset="utf8"');
+readfile('efl_feed.xml');
+print $xml->saveXML();
 eZExecution::cleanExit()
 
 
