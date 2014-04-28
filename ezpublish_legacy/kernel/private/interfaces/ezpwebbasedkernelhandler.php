566d5307a1e14a7dc9b5d74bbb6f8e7d70e91730
carlos.revillo <carlos.revillo@tantacom.com>
2014-04-28 09:54:34 +0200

    faltaban algunos archivos

---
+++ ezpublish_legacy/kernel/private/interfaces/ezpwebbasedkernelhandler.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * File containing the ezpWebBasedKernelHandler interface.
+ *
+ * @copyright Copyright (C) 1999-2014 eZ Systems AS. All rights reserved.
+ * @license http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License v2
+ * @version  2014.3
+ */
+
+/**
+ * Interface for web-based kernel handlers
+ */
+interface ezpWebBasedKernelHandler extends ezpKernelHandler
+{
+    /**
+     * Allows user to avoid executing the pagelayout template when running the kernel
+     *
+     * @param bool $usePagelayout
+     */
+    public function setUsePagelayout( $usePagelayout );
+}
