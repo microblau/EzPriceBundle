21d21c1bf98e45cade2c993144038e7c64461b8a
crevillo <crevillo@gmail.com>
2014-06-27 14:09:14 +0200

    Added Twig extension + Price service stubs

---
+++ API/Price/VatService.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * This file is part of the EzPriceBundle package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace EzSystems\EzPriceBundle\API\Price;
+
+interface VatService
+{
+}
---
+++ Core/Price/VatService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * This file is part of the EzPriceBundle package.
+ *
+ * @copyright Copyright (C) eZ Systems AS. All rights reserved.
+ * @license For full copyright and license information view LICENSE file distributed with this source code.
+ */
+
+namespace EzSystems\EzPriceBundle\Core\Price;
+
+use EzSystems\EzPriceBundle\API\Price\VatService as VatServiceInterface;
+
+class VatService implements VatServiceInterface
+{
+
+}
