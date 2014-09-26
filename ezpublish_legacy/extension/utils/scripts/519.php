3c2c84e6eaa4fc00684b0a410858e9aaf1629687
Carlos Revillo <crevillo@gmail.com>
2014-09-26 18:31:17 +0200

    un script para borrar un huerfano

---
+++ ezpublish_legacy/extension/utils/scripts/519.php
@@ -0,0 +1,22 @@
+<?php
+require 'autoload.php';
+$cli = eZCLI::instance();
+$script = eZScript::instance( array( 'description' => ( "eZ Publish (un)clusterize\n" .
+                                                        "Script for moving var_dir files from " .
+                                                        "filesystem to database and vice versa\n" .
+                                                        "\n" .
+                                                        "./bin/php/clusterize.php" ),
+                                     'use-session'    => false,
+                                     'use-modules'    => false,
+                                     'use-extensions' => true ) );
+
+$script->startup();
+
+$script->initialize();
+
+$object = eZContentObject::fetch( 519 );
+$object->purge();
+
+$script->shutdown();
+
+?>
