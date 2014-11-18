2e56d76658a25a6160166dc5ed22de3967934980
Sebastian Blank <sebastian.bl@gmx.de>
2014-11-18 23:08:57 +0100

    Fix wrong namespaces

--- Core/Persistence/Legacy/Price/Vat/AutomaticVatHandlerException.php
+++ Core/Persistence/Legacy/Price/Vat/AutomaticVatHandlerException.php
@@ -6,7 +6,7 @@
  * @license For full copyright and license information view LICENSE file distributed with this source code.
  */
 
-namespace EzSystems\EzPriceBundle\Core\Persistence\Legacy\Price\Gateway;
+namespace EzSystems\EzPriceBundle\Core\Persistence\Legacy\Price\Vat;
 
 use eZ\Publish\API\Repository\Exceptions\NotImplementedException;
 
